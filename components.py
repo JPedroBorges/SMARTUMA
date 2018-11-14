f72891c9238406d5df5417f21e6f72d942eb85d3
Joaquim Perez <jquim1@hotmail.com>
2018-11-14 23:17:02 +0000

    separate file for components

---
+++ components.py
@@ -0,0 +1,84 @@
+import subprocess
+import glob
+import time
+import threading
+
+"""base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+interface = "wlan0"""
+
+class SignalTranslator(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            """print("Wifi Signal: " + self.translate_signal(self.read_signal_raw()))"""
+            print('signal')
+            time.sleep(1)
+
+    def read_signal_raw(self):
+        proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
+        out, err = proc.communicate()
+
+        value = int(out.split("\n")[4].split("Signal level=")[1].split(" ")[0])
+
+        return value
+    
+    def translate_signal(self,value):
+        status = ""
+        if value > -30:
+           status = "Amazing"
+        elif value > -67:
+           status = "Very Good"
+        elif value > -70:
+           status = "Okay"
+        elif value > -80:
+           status = "Not Good"
+        elif value > -90:
+           status = "Unusable"
+        else:
+           status = "Do you even have internet?"
+           
+        return status     
+
+class TempReader(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            """print("Temperature: " + str(self.read_temp()) + "ยบC")"""
+            print('temp')
+            time.sleep(1)   
+
+    def read_temp_raw(self):
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+   
+    def read_temp(self):
+        lines = self.read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = self.read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c   
+
+class DeviceScanner(threading.Thread):
+
+        def __init__(self):
+            threading.Thread.__init__(self)
+
+        def run(self):
+            while True:
+              print("device")
+              time.sleep(1)                 
