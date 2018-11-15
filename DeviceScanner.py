7e4e22dd8b3fed405c1fd35f8767fc7c78792027
Joaquim Perez <jquim1@hotmail.com>
2018-11-15 22:27:51 +0000

    device scanner is now working (?)

--- DeviceScanner.py
+++ DeviceScanner.py
@@ -1,12 +1,21 @@
 import time
 import threading
 
+import os, re
+
 class DeviceScanner(threading.Thread):
 
         def __init__(self):
             threading.Thread.__init__(self)
 
         def run(self):
+
             while True:
               print("device")
+
+              full_results = [re.findall('^[\w\?\.]+|(?<=\s)\([\d\.]+\)|(?<=at\s)[\w\:]+', i) for i in os.popen('arp -a')]
+              final_results = [dict(zip(['IP', 'LAN_IP', 'MAC_ADDRESS'], i)) for i in full_results]
+              final_results = [{**i, **{'LAN_IP':i['LAN_IP'][1:-1]}} for i in final_results]
+              print(len(final_results))
+
               time.sleep(1)            
