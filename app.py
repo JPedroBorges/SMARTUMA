20ce7e92d41c80227dc3d2a977b6de7d05b0b24d
perezjquim <jquim1@hotmail.com>
2018-11-21 01:28:22 +0000

    cleanup

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
     
 if __name__ == "__main__":
-    """while True:
-        print("[ " + datetime.datetime.utcnow().strftime('%d %B %Y - %H:%M:%S') + " ]")
-        print("Wifi Signal: " + translate_signal(read_signal_raw()))
-        print("Temperature: " + str(read_temp()) + "ยบC")
-        print("-------------------------------")
-        
-        time.sleep(1)"""
-
     SignalTranslator().start()
     TempReader().start()
     DeviceScanner().start()
