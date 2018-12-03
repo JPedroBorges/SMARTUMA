2ee601f917a2d8fdfe83e3389dd370c6f0860a50
JPBorges <joao.borges@factory.odkas.com>
2018-12-03 22:54:14 +0000

    post authentication plus cors

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class SensorController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
+
     /**
      * Display a listing of the resource.
      *
