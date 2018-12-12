18876263511119f183fcfe306a35387237c414ef
JPBorges <joao.borges@factory.odkas.com>
2018-12-12 00:26:34 +0000

    authentication measures

--- app/Http/Controllers/MeasureController.php
+++ app/Http/Controllers/MeasureController.php
@@ -11,6 +11,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 class MeasureController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *
